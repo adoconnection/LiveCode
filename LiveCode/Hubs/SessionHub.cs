9bbff6bddddc84c28ca88a2bacc0dc15ad112b72
Alexnader Selishchev <ADOConnection@gmail.com>
2020-12-28 21:13:19 +0300

    initial commit

---
+++ LiveCode/Hubs/SessionHub.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveCode.Hubs
+{
+    public class SessionHub : Hub
+    {
+        private static ConcurrentDictionary<Guid, IList<string>> Sessions = new ConcurrentDictionary<Guid, IList<string>>();
+        private static ConcurrentDictionary<string, Guid> MembershipIndex = new ConcurrentDictionary<string, Guid>();
+        private static ConcurrentDictionary<Guid, string> SessionContent = new ConcurrentDictionary<Guid, string>();
+
+        public async Task Join(Guid sessionId)
+        {
+            IList<string> connections = Sessions.GetOrAdd(sessionId, new List<string>());
+
+            connections.Add(Context.ConnectionId);
+            MembershipIndex.TryAdd(Context.ConnectionId, sessionId);
+
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveContent", SessionContent.GetOrAdd(sessionId, ""), 0, 0);
+        }
+
+        public async Task UpdateContent(string content, int row, int pos)
+        {
+            if (!MembershipIndex.TryGetValue(Context.ConnectionId, out Guid session))
+            {
+                return;
+            }
+
+            SessionContent.AddOrUpdate(session, content, (a,b) => content);
+
+            foreach (string connection in Sessions[session])
+            {
+                if (connection == Context.ConnectionId)
+                {
+                    continue;
+                }
+
+                await Clients.Client(connection).SendAsync("ReceiveContent", content, row, pos);
+            }
+        }
+
+        public async Task UpdateCursor(int row, int pos)
+        {
+            if (!MembershipIndex.TryGetValue(Context.ConnectionId, out Guid session))
+            {
+                return;
+            }
+
+            foreach (string connection in Sessions[session])
+            {
+                if (connection == Context.ConnectionId)
+                {
+                    continue;
+                }
+
+                await Clients.Client(connection).SendAsync("ReceiveCursor", row, pos);
+            }
+        }
+
+        public async Task UpdateSelection(int srow, int spos, int erow, int epos)
+        {
+            if (!MembershipIndex.TryGetValue(Context.ConnectionId, out Guid session))
+            {
+                return;
+            }
+
+            foreach (string connection in Sessions[session])
+            {
+                if (connection == Context.ConnectionId)
+                {
+                    continue;
+                }
+
+                await Clients.Client(connection).SendAsync("ReceiveSelection", srow, spos, erow, epos);
+            }
+        }
+    }
+}
---
+++ LiveCode/wwwroot/js/session.js
@@ -0,0 +1,88 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/sessions").build();
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/textmate");
+editor.session.setMode("ace/mode/csharp");
+editor.setFontSize("12px");
+editor.setHighlightActiveLine('checked');
+editor.setHighlightSelectedWord(true);
+editor.setDisplayIndentGuides(true);
+editor.renderer.setShowPrintMargin(false);
+
+var isSilent = false;
+
+connection.on("ReceiveContent", function (content, row, pos) {
+    isSilent = true;
+    editor.getSession().setValue(content);
+    editor.selection.moveCursorTo(row, pos);
+    isSilent = false;
+});
+
+connection.on("ReceiveCursor", function (row, pos) {
+    isSilent = true;
+    editor.selection.moveCursorTo(row, pos);
+    isSilent = false;
+});
+
+connection.on("ReceiveSelection", function (srow, spos, erow, epos) {
+    isSilent = true;
+    editor.selection.setSelectionAnchor(srow, spos);
+    editor.selection.moveCursorTo(erow, epos);
+    isSilent = false;
+});
+
+connection.start().then(function ()
+{
+    connection.invoke("Join", SESSION_ID).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    editor.getSession().on('change', function () {
+
+        if (isSilent) {
+            return;
+        }
+
+        var cursor = editor.selection.getCursor();
+
+        connection.invoke("UpdateContent", editor.getSession().getValue(), cursor.row, cursor.column).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    });
+
+    editor.selection.on('changeCursor', function () {
+
+        if (isSilent) {
+            return;
+        }
+
+        var cursor = editor.selection.getCursor();
+
+        connection.invoke("UpdateCursor", cursor.row, cursor.column).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    });
+
+    editor.selection.on('changeSelection', function () {
+
+        if (isSilent) {
+            return;
+        }
+
+        var range = editor.selection.getRange();
+
+        connection.invoke("UpdateSelection", range.start.row, range.start.column, range.end.row, range.end.column).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    });
+
+
+}).catch(function (err)
+{
+    return console.error(err.toString());
+});
